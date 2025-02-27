ab82136ea7abfa2041c51483f6d3e1a38778fba6
David Bruner <david.bruner@abley.com>
2025-02-27 22:16:39 +1300

    add env variable for api url

--- src/services/ApiHandler.ts
+++ src/services/ApiHandler.ts
@@ -9,7 +9,7 @@ import {
 } from "../data/Interfaces";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8000',
 });
 
 let currentSessionId: string | null = null;
