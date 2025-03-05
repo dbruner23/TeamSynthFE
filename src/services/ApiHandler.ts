ac32becf62fa774282c3909c0788839eaec9c95e
David Bruner <david.bruner@abley.com>
2025-03-06 09:59:04 +1300

    change back api url

--- src/services/ApiHandler.ts
+++ src/services/ApiHandler.ts
@@ -10,7 +10,7 @@ import {
 import { TaskCancellationResponse } from "../data/TaskInterfaces";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", //"https://teamsynthbe.onrender.com/api",
+  baseURL: "https://teamsynthbe.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
